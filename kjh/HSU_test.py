9577192afd854a5f01b2a16f661a7bccb66c1d52
kimjunodes <rla04790@gmail.com>
2022-01-13 21:44:39 +0900

    Update HSU_test.py
    혹시몰라서 마지막 데모 테스트

--- kjh/HSU_test.py
+++ kjh/HSU_test.py
@@ -12,6 +12,8 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
+print(__file__)
+
 devices = AudioUtilities.GetSpeakers()  # 오디오 받아오기
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
