70971d686c330fd1f7487a47f37e86e33bac164d
Hoony97 <87511271+Hoony97@users.noreply.github.com>
2022-01-24 16:20:46 +0900

    Jesus
    Jesus

--- ksh/sample.py
+++ ksh/sample.py
@@ -1,31 +1,38 @@
 import cv2
+from torch import rand
 import streamlit as st
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import random
+
+devices = AudioUtilities.GetSpeakers()  # 오디오 받아오기
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
 
 st.title("Webcam Test")
 
-rad = st.sidebar.radio("Solution",["A", "B", "C"])
-ckb1 = st.sidebar.checkbox("A")
-ckb2 = st.sidebar.checkbox("B")
-ckb3 = st.sidebar.checkbox("C")
+rad = st.sidebar.radio("Solution",["MAIN"])
 FRAME_WINDOW = st.image([])
+ss = st.sidebar.slider("ABC",0,100)
+print(volume)
 
-if rad == "A":
-    st.write("A")
-    if st.button("Button") == True:
-        rad = "B"
+if rad == "MAIN":
+    bt1 = st.button("ButtonA")
+    bt2 = st.button("ButtonB")
+    bt3 = st.button("ButtonC")
+
+    if bt1:
+        st.write("A")
+        rad = st.sidebar.radio("Solution",["A"])
+
     
-if rad == "B":
-    st.write("B")
+    if bt2:
+        st.write("B")
+        rad = st.sidebar.radio("Solution",["B"])
 
-if rad == "C":
-    st.write("C")
-
-
-if ckb1 == True:
-    st.write("A")
-
-if ckb2 == "B":
-    st.write("B")
-
-if ckb3 == "C": 
-    st.write("C")
+   
+    if bt3 == True:
+        st.write("C")
+        rad = st.sidebar.radio("Solution",["C"])    
